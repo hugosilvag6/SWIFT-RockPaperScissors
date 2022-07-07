36b0159495d3e6c15e18373749eb43b8dd8c95f1
hugosilvag6 <hugosilvag6@hotmail.com>
2022-07-06 23:47:47 -0300

    Initial Commit

---
+++ rockPaperScissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rockPaperScissors
+//
+//  Created by Hugo Silva on 06/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ rockPaperScissors/rockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rockPaperScissorsApp.swift
+//  rockPaperScissors
+//
+//  Created by Hugo Silva on 06/07/22.
+//
+
+import SwiftUI
+
+@main
+struct rockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
